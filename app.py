6885b6bf9d1e89819d533942bfffdab64bbbc65a
Joe Meilinger <joemeilinger@gmail.com>
2017-04-27 13:54:02 -0500

    Add env var testing

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import editdistance
+import os
+import pylev
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, catch_all_404s=True)
 
 
 class EditDistance(Resource):
@@ -14,8 +15,12 @@ class EditDistance(Resource):
 
     def get(self):
         args = self.parser.parse_args()
-        distance = editdistance.eval(args.get('str1'), args.get('str2'))
-        return {'distance': distance}
+        distance = pylev.levenschtein(args.get('str1'), args.get('str2'))
+        print 'Distance from {} to {} is {}'.format(args.get('str1'), args.get('str2'), distance)
+        print os.environ
+        return {
+            'distance': distance
+        }
 
 
 api.add_resource(EditDistance, '/edit-distance/')
